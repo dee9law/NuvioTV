bc75a31b2640219578c4fae361a9623eb8e230c1
skoruppa <skoruppa@gmail.com>
2026-05-14 09:51:39 +0200

    Simple failsafe to not remove local progress of items not yet pushed

--- app/src/main/java/com/nuvio/tv/ui/screens/player/PlayerRuntimeControllerPlaybackEvents.kt
+++ app/src/main/java/com/nuvio/tv/ui/screens/player/PlayerRuntimeControllerPlaybackEvents.kt
@@ -275,7 +275,7 @@ internal fun PlayerRuntimeController.saveWatchProgressInternal(position: Long, d
         progressPercent = fallbackPercent
     )
 
-    scope.launch {
+    scope.launch(kotlinx.coroutines.NonCancellable) {
         watchProgressRepository.saveProgress(progress, syncRemote = syncRemote)
     }
 }
